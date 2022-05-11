0143ad4f4b789c8045d866860590d8e3d222916d
Martim <martim.pintao@gmail.com>
2022-05-11 17:07:14 +0100

    Create game() function and fix typos
    Create game() function that lets the user play a 5 rounds game against the computer.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,37 +18,52 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-let computerSelection = computerPlay()
+let computerSelection
 
-let playerSelection = prompt("Rock, paper, or scissors?", "").toLocaleLowerCase()
+let playerSelection
+
+let playerScore = 0
+
+let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
+
+    computerSelection = computerPlay()
+
+    playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase()
+
     if (playerSelection == computerSelection) {
         return ("It's a tie!")
     }
     
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return ("Congrats, you win! The rock beaten the scissors")
+        playerScore = (playerScore + 1)
+        return ("Congrats, you won! The rock beaten the scissors");
     }
 
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        return ("Congrats, you win! The paper beaten the rock")
+        playerScore = (playerScore + 1)
+        return ("Congrats, you won! The paper beaten the rock")
     }
 
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return ("Congrats, you win! The scissors beaten the paper")
+        playerScore = (playerScore + 1)
+        return ("Congrats, you won! The scissors beaten the paper")
     }
 
     else if (computerSelection == "rock" && playerSelection == "scissors") {
-        return ("Oh no, you lose! The rock beaten the scissors")
+        computerScore = (computerScore + 1)
+        return ("Oh no, you lost! The rock beaten the scissors")
     }
 
     else if (computerSelection == "paper" && playerSelection == "rock") {
-        return ("Oh no, you lose! The paper beaten the rock")
+        computerScore = (computerScore + 1)
+        return ("Oh no, you lost! The paper beaten the rock")
     }
 
     else if (computerSelection == "scissors" && playerSelection == "paper") {
-        return ("Oh no, you lose! The scissors beaten the paper")
+        computerScore = (computerScore + 1)
+        return ("Oh no, you lost! The scissors beaten the paper")
     }
 
     else {
@@ -56,4 +71,24 @@ function playRound(playerSelection, computerSelection) {
     }
 
 }
+
+function game() {
+    
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection).concat(" - ", playerScore, "-", computerScore));
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You won the game! The final result is ", playerScore, "-", computerScore)
+    }
+
+    else if (playerScore < computerScore) {
+        console.log("Bad luck, you lost the game! The final result is ", playerScore, "-", computerScore)
+    }
+
+    else {
+        console.log("It's a tie! The final result is ", playerScore, "-", computerScore)
+    }
+}
+
 
